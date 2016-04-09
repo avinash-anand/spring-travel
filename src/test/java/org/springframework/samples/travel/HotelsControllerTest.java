6e58b4673fec758e486a06465ee3ce8586379b23
avinash anand <avinash.mait@gmail.com>
2016-04-09 15:32:15 +0100

    added ignore to see passing tests

--- src/test/java/org/springframework/samples/travel/HotelsControllerTest.java
+++ src/test/java/org/springframework/samples/travel/HotelsControllerTest.java
@@ -2,51 +2,61 @@ package org.springframework.samples.travel;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class HotelsControllerTest {
 
 	@Test
+	@Ignore
 	public void testHotelsController() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testSearch() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testList() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testShow() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testEnterBookingDetails() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testReviewBookingDetails() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testBooking() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testBookingSuccess() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testDeleteBooking() {
 		fail("Not yet implemented");
 	}
